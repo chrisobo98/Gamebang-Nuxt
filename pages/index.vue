5fa930f44135424f65130e89bfaf80be8867c58d
chrisobo98 <christopherbermudez98@gmail.com>
2020-09-29 14:21:28 -0400

    to fetch the IGDB api data I brought in some handy dandy code from the nuxt guide/async-data so that I can render it

--- pages/index.vue
+++ pages/index.vue
@@ -28,7 +28,30 @@
 </template>
 
 <script>
-export default {}
+
+export default {
+  components : {
+    Logo
+  },
+  created() {
+    games = response.data
+  },
+  asyncData({ params, error }) {
+    return axios
+      .get(`https://my-api/posts/${params.id}`)
+      .then(res => {
+        return { title: res.data.title }
+      })
+      .catch(e => {
+        error({ statusCode: 404, message: 'Post not found' })
+      })
+  },
+  data() {
+    return {
+      
+    }
+  }
+}
 </script>
 
 <style>
