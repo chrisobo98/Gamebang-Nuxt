4bf415576fa5613d8891d7eb9e1abf8e93f244f4
chrisobo98 <christopherbermudez98@gmail.com>
2020-09-29 20:40:43 -0400

    added a line of code in config, generate fallback true

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'GameBang | Where Gamers Get Their Information',
+    title: 'HypeGames | Where Gamers Get Their Information',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -41,5 +41,9 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+  },
+
+  generate: {
+    fallback: true
   }
 }
