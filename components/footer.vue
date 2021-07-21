cb05281972e518951b544692563fbfb76b528bfe
chrisobo98 <christopherbermudez98@gmail.com>
2021-07-21 10:36:19 -0400

    noice custom footer

--- components/footer.vue
+++ components/footer.vue
@@ -1,38 +1,36 @@
 <template>
   <v-footer dark padless>
-    <v-card flat tile class="indigo lighten-1 white--text text-center">
-      <v-card-text>
-        <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" icon>
-          <v-icon size="24px">
-            {{ icon }}
-          </v-icon>
-        </v-btn>
-      </v-card-text>
+    <v-card-text class="justify-center d-flex">
+      <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" icon>
+        <v-icon size="24px">
+          {{ icon }}
+        </v-icon>
+      </v-btn>
+    </v-card-text>
+    <v-row justify="center" no-gutters>
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        text
+        rounded
+        class="my-2"
+      >
+        {{ link }}
+      </v-btn>
 
-      <v-card-text class="white--text pt-0">
-        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet.
-        Mauris cursus commodo interdum. Praesent ut risus eget metus luctus
-        accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a
-        sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula
-        lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus
-        iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor
-        vel ut orci. Orci varius natoque penatibus et magnis dis parturient
-        montes, nascetur ridiculus mus.
+      <v-card-text class="white--text justify-center d-flex">
+        {{ new Date().getFullYear() }} GameHang —
+        <strong class="ml-1"> Made with ❤️ in Central Florida</strong>
       </v-card-text>
-
-      <v-divider></v-divider>
-
-      <v-card-text class="white--text">
-        {{ new Date().getFullYear() }} —
-        <strong>Made with ❤️ in Central Florida</strong>
-      </v-card-text>
-    </v-card>
+    </v-row>
   </v-footer>
 </template>
 <script>
 export default {
   data: () => ({
-    icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"]
+    icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"],
+    links: ["Home", "About Us", "Team", "Services", "Blog", "Contact Us"]
   })
 };
 </script>
